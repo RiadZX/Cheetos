aada54f7cdfdaf25de41c146e14aead9aa618da6
Roan Laanen <laanen.roan@gmail.com>
2021-04-15 22:36:30 +0200

    Added a text input field for the code

--- ProjectCheetos/MainWindow.xaml.cs
+++ ProjectCheetos/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace ProjectCheetos
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
